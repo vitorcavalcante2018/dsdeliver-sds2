245590e6ce69c6fbb848029e8ee442418eb20a5f
unknown <vitor.cavalcante@redetuxnet.com.br>
2021-01-04 20:50:17 -0300

    Start Project

---
+++ beckend/src/test/java/com/devesuperior/dsdeliveri/DsdeliveriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dsdeliveri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
